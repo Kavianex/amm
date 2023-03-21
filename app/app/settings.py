113bac0a62c6af854181bcd7e77fcfad6dc94738
Hadi Jamshidi <m.hadi.jamshidi@gmail.com>
2023-03-21 20:33:52 +0100

    downgrade django

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '04017))5hdjfkg@%fza!f@ut3ghc9)%11wd7v)_pgk8rw#=k5h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
 
